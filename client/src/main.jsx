e74fd7b66ba1622df6dcc03d4a56b453df3c9d3d
vika2204 <viktoriya.kireeva22@gmail.com>
2024-11-08 14:20:03 +0300

    client app created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import TopicList from "./components/TopicList.jsx";
+import FlashcardList from "./components/FlashcardList.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/topics/:topicId",
+                element: <FlashcardList />,
+            },
+            {
+                path: "/",
+                element: <TopicList />,
+            },
+        ]
+
+    },
+
+]);
+
+
+createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
